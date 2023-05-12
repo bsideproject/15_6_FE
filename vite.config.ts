21ac2f5494932050a27e080400e4c6a9e96b439d
kyu <2066apple@naver.com>
2023-05-12 22:47:54 +0900

    ➕ vite plugin pwa 설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            },
+        }),
+    ],
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
