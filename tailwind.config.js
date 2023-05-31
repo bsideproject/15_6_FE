c17ffea4fb5543c60e34c63875fed780e7807936
kyu <2066apple@naver.com>
2023-05-31 22:26:24 +0900

    [ chore ] 폰트, 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            primary: '#FFD21D',
+            ['primary-dark']: '#FCA10F',
+            ['primary-light']: '#FFE747',
+            accent: '#1D6AFF',
+            negative: '#FF2B1D',
+            warning: '#FFC01D',
+            postive: '#0BD15B',
+        },
+        extend: {
+            fontFamily: {
+                ['suit-regular']: ['SUIT-Regular'],
+                ['suit-bold']: ['SUIT-Bold'],
+            },
+        },
     },
     plugins: [],
 };
