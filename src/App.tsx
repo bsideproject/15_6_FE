583f758744e23ad723495f5cf8ee63f7216c8dda
kyu <2066apple@naver.com>
2023-06-03 00:46:31 +0900

    [ feat ] 바텀 네비바 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import '@/styles/global.scss';
 import { Routes } from '@/routes';
+import { useIsLayout } from '@/utils/location';
+import DefaultLayout from '@/components/layout/DefaultLayout';
 
 function App() {
+    const isLayout = useIsLayout();
     return (
         <>
+            {isLayout ? <DefaultLayout /> : null}
             <Routes />
         </>
     );
