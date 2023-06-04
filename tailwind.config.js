ed10dcba7230bdc7f740e93810fed0b026447454
kyu <2066apple@naver.com>
2023-06-04 11:25:35 +0900

    [ style ] 색상, 글씨체 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,20 @@ export default {
             negative: '#FF2B1D',
             warning: '#FFC01D',
             postive: '#0BD15B',
+            'gray-0': '#FFFFFF',
+            'gray-50': '#F5F5F5',
+            'gray-100': '#EEEEEE',
+            'gray-300': '#D5D5D5',
+            'gray-500': '#A2A2A2',
+            'gray-600': '#6F6F6F',
+            'gray-700': '#494949',
+            'gray-900': '#090909',
         },
         extend: {
             fontFamily: {
                 ['suit-regular']: ['SUIT-Regular'],
                 ['suit-bold']: ['SUIT-Bold'],
+                ['suit-medium']: ['SUIT-Medium'],
             },
             boxShadow: {
                 normal: '0px -1px 18px rgba(0, 0, 0, 0.07)',
