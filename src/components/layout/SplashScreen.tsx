b113936143c648c1f24adf3e0fcd90a48a416d65
kyu <2066apple@naver.com>
2023-06-20 00:02:20 +0900

    [ style ] 스플래시 이미지 변경 및 스타일 변경

--- src/components/layout/SplashScreen.tsx
+++ src/components/layout/SplashScreen.tsx
@@ -1,8 +1,20 @@
-import { ReactComponent as Splash } from '@/assets/img/splash_logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { ReactComponent as Splash } from '@/assets/img/img_splash.svg';
 export const SplashScreen = () => {
+    const splashRef = useRef<HTMLDivElement>(null);
+    const [marginTop, setMarginTop] = useState<string>('');
+
+    useEffect(() => {
+        if (splashRef.current?.clientHeight) {
+            setMarginTop('mt-[' + splashRef.current.clientHeight * 0.3125 + 'px]');
+        }
+    }, []);
+
     return (
-        <div className="fixed top-0 left-0 w-full h-screen flex justify-center items-center bg-gray-0">
-            <Splash></Splash>
+        <div className="fixed top-0 left-0 w-full h-screen flex justify-center bg-gray-0">
+            <div className={`h-full ${marginTop}`} ref={splashRef}>
+                <Splash></Splash>
+            </div>
         </div>
     );
 };
