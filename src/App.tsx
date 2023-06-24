04dbb1e046fdb5a1251ed03f89b3ed89e13e7ec4
2016026480 <junslim11@gmail.com>
2023-06-25 00:31:09 +0900

    feat: 스플래시 스크린 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import '@/styles/global.scss';
 import { Routes } from '@/routes';
 import DefaultLayout from '@/components/layout/DefaultLayout';
-import { SplashScreen } from '@/components/layout/SplashScreen';
-import { useEffect, useState } from 'react';
 
 function App() {
-    const [splashLoading, setSplashLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setSplashLoading(false);
-        }, 1000);
-    }, []);
-
     return (
-        <>
-            <DefaultLayout />
-            {splashLoading ? <SplashScreen /> : null}
-            <Routes />
-        </>
+        <div className="select-none">
+            <DefaultLayout>
+                <Routes />
+            </DefaultLayout>
+        </div>
     );
 }
 
