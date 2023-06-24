81ab84ed0ad8a5cf22b04b74563a2841643fae4c
kyu <2066apple@naver.com>
2023-06-24 20:21:39 +0900

    [ docs ] 팝업 문서

--- src/components/popup/Popup.stories.tsx
+++ src/components/popup/Popup.stories.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 
 import { Popup } from './Popup';
 import { AlertPopup } from './PopupGroup';
