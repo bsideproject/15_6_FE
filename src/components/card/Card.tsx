2f7c774fcaeaf6bc78516871c6724ec84f94ce14
kyu <2066apple@naver.com>
2023-06-28 22:53:17 +0900

    [ feat ] 카드 컴포넌트 추가

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,30 @@
+import { Tag } from '@/components/tag/Tag';
+import { ReactComponent as More } from '@/assets/img/icn_more.svg';
+
+interface CardProps {
+    title: string;
+    goals?: string;
+    startDate: Date;
+    endDate: Date;
+    openMenu?: React.MouseEventHandler<HTMLDivElement | HTMLButtonElement> | undefined;
+    onClick?: React.MouseEventHandler<HTMLDivElement | HTMLButtonElement> | undefined;
+    className?: string;
+}
+
+export const Card = (props: CardProps) => {
+    const { title, goals, openMenu, startDate, endDate, className } = props;
+    return (
+        <div
+            className={`card w-full min-h-[128px] flex flex-col jusitfy-center px-[20px] py-[24px] bg-white ${className}`}
+        >
+            <div className="flex justify-between mb-[8px]">
+                <Tag startDate={startDate} endDate={endDate} />
+                <div onClick={openMenu}>
+                    <More />
+                </div>
+            </div>
+            <div className="title2 mb-[2px] line-clamp-2">{title}</div>
+            <div className="body2 text-gray-600 line-clamp-1">{goals}</div>
+        </div>
+    );
+};
