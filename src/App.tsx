95a9b1f9ea9c3482ba133d1d62b2590ef86b23bc
kyu <2066apple@naver.com>
2023-07-01 11:25:39 +0900

    [ feat ] 토스트 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import '@/styles/global.scss';
 import { Routes } from '@/routes';
 import DefaultLayout from '@/components/layout/DefaultLayout';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
     return (
         <div className="select-none">
             <DefaultLayout>
                 <Routes />
+                <Toaster position="bottom-center" containerStyle={{ bottom: 56 + 12 }} />
             </DefaultLayout>
         </div>
     );
