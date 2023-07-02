97750d56fd88bb6f5523a611af5b199c802ff20d
2016026480 <junslim11@gmail.com>
2023-07-02 16:37:51 +0900

    feat: add kakao sdk init, remove react StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/styles/reset.css';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 
+declare global {
+    interface Window {
+        Kakao: any;
+    }
+}
+
+window.Kakao.init('998580566d5e913c0107f31723601874');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <RecoilRoot>
-                <App />
-            </RecoilRoot>
-        </BrowserRouter>
-    </React.StrictMode>,
+    <BrowserRouter>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    </BrowserRouter>,
 );
