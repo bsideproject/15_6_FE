66d6d16bd69071b6385064ad07704df3d9efc939
kyu <2066apple@naver.com>
2023-07-05 23:51:13 +0900

    [ feat ] 헤더 레이아웃에서 사용하는 유틸 함수 수정

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -1,10 +1,20 @@
 import { useLocation } from 'react-router-dom';
-import { headerHiddenPaths, bottomNavBarHiddenPaths, routeTitle } from '@/constant/route';
+import {
+    headerHiddenPaths,
+    bottomNavBarHiddenPaths,
+    routeTitle,
+    headerBackPaths,
+    headerClosePaths,
+} from '@/constant/route';
 
 export const useIsBack = () => {
     const location = useLocation();
-    const isBack = location.pathname.split('/').length > 2;
-    return isBack;
+    return headerBackPaths.some((path) => location.pathname.startsWith(path));
+};
+
+export const useIsClose = () => {
+    const location = useLocation();
+    return headerClosePaths.some((path) => location.pathname.startsWith(path));
 };
 
 export const useHasHeader = () => {
@@ -17,15 +27,23 @@ export const useHasBottomNavBar = () => {
     return !bottomNavBarHiddenPaths.some((path) => location.pathname.includes(path));
 };
 
-export const useTitle = (lang: 'en' | 'ko') => {
+export const useTitle = (lang: 'en' | 'ko'): [string, boolean] => {
     const location = useLocation();
-    const result = routeTitle.find((route) => route.path === location.pathname);
+
+    const result = routeTitle.find((route) => {
+        // 예외 사항
+        if (location.pathname.startsWith('/nottodo/edit') && route.path === '/nottodo/edit') {
+            return route;
+        }
+        return location.pathname === route.path;
+    });
+
     if (result) {
         if (lang === 'ko') {
-            return result.ko;
+            return [result.ko, result.img];
         } else {
-            return result.en;
+            return [result.en, result.img];
         }
     }
-    return '';
+    return ['', false];
 };
