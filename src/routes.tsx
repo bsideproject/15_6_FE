af1a6895b45d8da36144a1efb3047c0dfe964297
kyu <2066apple@naver.com>
2023-07-05 23:52:32 +0900

    [ feat ] 낫투두 등록, 수정 페이지 레이아웃 완료

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Routes as Switch, Route } from 'react-router-dom';
 import HomePage from '@/pages/home';
 import LoginPage from '@/pages/login';
 import NotTodoPage from '@/pages/nottodo';
+import NotTodoCreatePage from './pages/nottodo/NotTodoCreatePage';
 import BadgePage from '@/pages/badge';
 import ProfilePage from '@/pages/profile';
 import SignUpPage from './pages/signup';
@@ -15,6 +16,8 @@ export const Routes = () => {
             <Route path={'/login'} element={<LoginPage />} />
             <Route path={'/signup'} element={<SignUpPage />} />
             <Route path={'/nottodo'} element={<NotTodoPage />} />
+            <Route path={'/nottodo/create'} element={<NotTodoCreatePage />} />
+            <Route path={'/nottodo/edit/:id'} element={<NotTodoCreatePage />} />
             <Route path={'/badge'} element={<BadgePage />} />
             <Route path={'/profile'} element={<ProfilePage />} />
         </Switch>
