f3b1800616634d8e8f3b61a0c6f2313c4ef16f25
kyu <2066apple@naver.com>
2023-07-05 23:50:13 +0900

    [ feat ] 헤더 레이아웃 경로 마다 나타나는 기능 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as Logo } from '@/assets/img/icn_logo_medium.svg';
 interface HeaderChildProps {
     children: React.ReactNode;
 }
@@ -5,18 +6,20 @@ interface HeaderChildProps {
 export interface HeaderProps extends HeaderChildProps {
     height: number;
     title?: string;
+    isTitleImg?: boolean;
 }
 
 export const Header = (props: HeaderProps) => {
-    const { children, title, height } = props;
+    const { children, title, height, isTitleImg } = props;
     return (
         <>
             <div
-                className={`header-wrapper top-0 left-0 w-full px-[20px] bg-white flex justify-between`}
+                className={`header-wrapper relative top-0 left-0 w-full px-[20px] bg-white flex justify-between`}
                 style={{ height: height }}
             >
-                <div className="absolute w-1/2 top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2 truncate title1">
-                    {title}
+                <div className="absolute w-1/2 top-1/2 left-1/2 flex justify-center items-center -translate-x-1/2 -translate-y-1/2 truncate title1">
+                    {isTitleImg ? <Logo className="w-[94px] h-[38px] px-1" /> : null}
+                    <span className="px-1">{title}</span>
                 </div>
                 {children}
             </div>
