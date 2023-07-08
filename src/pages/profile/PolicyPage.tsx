fc86569380969e515501d4cd06b1d574bf7afc02
kyu <2066apple@naver.com>
2023-07-08 22:54:25 +0900

    [ feat ] 정책관련 임베드 페이지 추가

---
+++ src/pages/profile/PolicyPage.tsx
@@ -0,0 +1,4 @@
+import ReactNotion from '@/utils/reactNotion';
+export default function PolicyPage() {
+    return <div className="w-full px-5">{ReactNotion('43d354ae6c504f259fd434668be9cef1')}</div>;
+}
