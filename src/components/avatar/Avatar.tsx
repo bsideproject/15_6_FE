35c55613f3c094bdd52034a333cf6332cccb5fdc
kyu <2066apple@naver.com>
2023-07-08 21:17:13 +0900

    [ feat ] 아바타 컴포넌트 추가

---
+++ src/components/avatar/Avatar.tsx
@@ -0,0 +1,17 @@
+interface AvatarProps {
+    size?: 'md' | 'lg';
+    src: string;
+}
+
+export const Avatar = (props: AvatarProps) => {
+    const { size, src } = props;
+    const sizeClass = (size: 'md' | 'lg') => {
+        if (size === 'lg') return 'w-[100px] h-[100px]';
+        else return 'w-[64px] h-[64px]';
+    };
+    return (
+        <div className={`${sizeClass(size ?? 'md')} rounded-full flex items-center justify-center`}>
+            <img className="w-full h-full rounded-full" src={src} alt="avatar image" />
+        </div>
+    );
+};
