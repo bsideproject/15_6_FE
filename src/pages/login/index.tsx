84a525f380aa6c3f462650541196427f57d7c248
2016026480 <junslim11@gmail.com>
2023-07-09 15:04:37 +0900

    feat: disable google and apple login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import KakaoLoginButton from '@/components/buttons/SocialLoginButtons/KakaoLoginButton';
-import AppleLoginButton from '@/components/buttons/SocialLoginButtons/AppleLoginButton';
-import GoogleLoginButton from '@/components/buttons/SocialLoginButtons/GoogleLoginButton';
+// import AppleLoginButton from '@/components/buttons/SocialLoginButtons/AppleLoginButton';
+// import GoogleLoginButton from '@/components/buttons/SocialLoginButtons/GoogleLoginButton';
 import { ReactComponent as LoginContent } from '@/assets/img/img_login_content.svg';
 import { getUserOS } from '@/utils/userAgent';
 
@@ -11,8 +11,8 @@ export default function LoginPage() {
         <div className="h-screen flex flex-col justify-center">
             <LoginContent className="mx-auto mb-5" />
             <KakaoLoginButton />
-            {isAppleUser() && <AppleLoginButton />}
-            <GoogleLoginButton />
+            {/* {isAppleUser() && <AppleLoginButton />} */}
+            {/* <GoogleLoginButton /> */}
         </div>
     );
 }
