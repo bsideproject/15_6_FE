21c8c4d87b9e632ee95069dac008dccd1ed4cb4e
kyu <2066apple@naver.com>
2023-07-17 22:32:57 +0900

    [ feat ] 알림 페이지 추가

---
+++ src/components/scrollable-picker/ScrollablePicker.tsx
@@ -0,0 +1,185 @@
+import { useEffect, useState, useRef } from 'react';
+
+const apm = ['오전', '오후'];
+const hour = Array.from(Array(12).keys());
+const min = Array.from(Array(60).keys());
+
+interface ScrollablePicker {
+    time: string;
+    setTime: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const ScrollablePicker = (props: ScrollablePicker) => {
+    const { time, setTime } = props;
+    const [position, setPosition] = useState(0);
+    const [currentHourIndex, setCurrentHourIndex] = useState<number>(0);
+    const [currentMinIndex, setCurrentMinIndex] = useState<number>(0);
+    const [currentApmIndex, setCurrentApmIndex] = useState<number>(0);
+    const hourRef = useRef<HTMLDivElement>(null);
+    const minRef = useRef<HTMLDivElement>(null);
+    const apmRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const itemHeight = 20;
+        let timer: any = null;
+
+        const apmStartScroll = () => {
+            // 스크롤 위치 계산
+            const scrollPosition = apmRef.current?.scrollTop || 0;
+            setPosition(scrollPosition);
+
+            // 가장 가까운 항목 계산
+            const closestItemIndex = Math.round(scrollPosition / itemHeight);
+            setCurrentApmIndex(closestItemIndex);
+
+            // 이전에 설정한 타이머가 있다면 취소
+            if (timer) {
+                clearTimeout(timer);
+            }
+            // 스크롤 이벤트가 종료되었을 때 가장 가까운 항목으로 이동
+            timer = setTimeout(() => {
+                apmRef.current?.scrollTo({
+                    top: closestItemIndex * itemHeight,
+                    behavior: 'smooth',
+                });
+            }, 100); // 100ms 동안 스크롤이 없으면 스크롤 이벤트 종료로 간주
+        };
+
+        const hourStartScroll = () => {
+            // 스크롤 위치 계산
+            const scrollPosition = hourRef.current?.scrollTop || 0;
+            setPosition(scrollPosition);
+
+            // 가장 가까운 항목 계산
+            const closestItemIndex = Math.round(scrollPosition / itemHeight);
+            setCurrentHourIndex(closestItemIndex);
+
+            // 이전에 설정한 타이머가 있다면 취소
+            if (timer) {
+                clearTimeout(timer);
+            }
+            // 스크롤 이벤트가 종료되었을 때 가장 가까운 항목으로 이동
+            timer = setTimeout(() => {
+                hourRef.current?.scrollTo({
+                    top: closestItemIndex * itemHeight,
+                    behavior: 'smooth',
+                });
+            }, 100); // 100ms 동안 스크롤이 없으면 스크롤 이벤트 종료로 간주
+        };
+
+        const minStartScroll = () => {
+            // 스크롤 위치 계산
+            const scrollPosition = minRef.current?.scrollTop || 0;
+            setPosition(scrollPosition);
+
+            // 가장 가까운 항목 계산
+            const closestItemIndex = Math.round(scrollPosition / itemHeight);
+            setCurrentMinIndex(closestItemIndex);
+
+            // 이전에 설정한 타이머가 있다면 취소
+            if (timer) {
+                clearTimeout(timer);
+            }
+            // 스크롤 이벤트가 종료되었을 때 가장 가까운 항목으로 이동
+            timer = setTimeout(() => {
+                minRef.current?.scrollTo({
+                    top: closestItemIndex * itemHeight,
+                    behavior: 'smooth',
+                });
+            }, 100); // 100ms 동안 스크롤이 없으면 스크롤 이벤트 종료로 간주
+        };
+
+        apmRef.current?.addEventListener('scroll', apmStartScroll);
+        hourRef.current?.addEventListener('scroll', hourStartScroll);
+        minRef.current?.addEventListener('scroll', minStartScroll);
+        return () => {
+            apmRef.current?.addEventListener('scroll', apmStartScroll);
+            hourRef.current?.addEventListener('scroll', hourStartScroll);
+            minRef.current?.addEventListener('scroll', minStartScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const Hour = hour[currentHourIndex] + 1;
+        const time = apm[currentApmIndex] + ' ' + Hour + '시' + ' ' + min[currentMinIndex] + '분';
+        setTime(time);
+    }, [currentApmIndex, currentHourIndex, currentMinIndex]);
+
+    const rotateClass = (divIndex: number, curIndex: number) => {
+        const index = divIndex - curIndex;
+        switch (index) {
+            // case -3:
+            //     return 'rotate-up-1';
+            case -2:
+                return 'rotate-up-1';
+            case -1:
+                return 'rotate-up-2';
+            case 1:
+                return 'rotate-down-1';
+            case 2:
+                return 'rotate-down-2';
+            // case 3:
+            //     return 'rotate-down-3';
+            default:
+                break;
+        }
+    };
+
+    return (
+        <div className="wrapper w-full h-[100px] overflow-hidden relative body1">
+            {/* <div className="divider absolute top-1/3 w-full h-[1px] bg-black"></div>
+            <div className="divider absolute top-2/3 w-full h-[1px] bg-black"></div> */}
+            <div className="viewer w-full h-full flex px-12">
+                <div ref={apmRef} className="apm flex flex-col w-1/3 items-center overflow-y-scroll hide-scroll">
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    {apm.map((v, index) => (
+                        <div
+                            key={'apm' + index}
+                            className={`h-[20px] shrink-0 flex justify-center items-center ${
+                                currentApmIndex !== index ? 'text-gray-300' : ''
+                            } ${rotateClass(index, currentApmIndex)}`}
+                        >
+                            {v}
+                        </div>
+                    ))}
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                </div>
+                <div ref={hourRef} className="hour flex flex-col w-1/3 items-center overflow-y-scroll hide-scroll">
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    {hour.map((v, index) => (
+                        <div
+                            key={'hour' + index}
+                            className={`h-[20px] shrink-0 flex justify-center items-center ${
+                                currentHourIndex !== index ? 'text-gray-300' : ''
+                            } ${rotateClass(index, currentHourIndex)}`}
+                        >
+                            {v + 1}
+                        </div>
+                    ))}
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                </div>
+                <div ref={minRef} className="hour flex flex-col w-1/3 items-center overflow-y-scroll hide-scroll">
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    {min.map((v, index) => (
+                        <div
+                            key={'min' + index}
+                            className={`h-[20px] shrink-0 flex justify-center items-center ${
+                                currentMinIndex !== index ? 'text-gray-300' : ''
+                            } ${rotateClass(index, currentMinIndex)}`}
+                        >
+                            {index < 10 ? '0' + v : v}
+                        </div>
+                    ))}
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                    <div className="h-[20px] shrink-0 flex justify-center items-center" />
+                </div>
+            </div>
+            <div className="divider absolute top-[35%] left-0 w-full h-[30px] bg-gray-50 rounded-lg -z-10"></div>
+        </div>
+    );
+};
--- src/pages/profile/AlarmPage.tsx
+++ src/pages/profile/AlarmPage.tsx
@@ -6,17 +6,29 @@ export default function AlarmPage() {
     // TODO 유저가 알람 설정해놨으면 그걸로 세팅
     const [isAlarm, setIsAlarm] = useState<boolean>(false);
     const [isTime, setIsTime] = useState<boolean>(false);
-    const [time, setTime] = useState<string>('');
+    const [currentTime, setCurrentTime] = useState<string>('오전 1시 0분');
+    const [prevTime, setPrevTime] = useState<string>('오전 1시 0분');
 
     const handleAlarm = () => {
         setIsAlarm(!isAlarm);
         // TODO 알림 설정 관련 api
         // TODO 사용자가 설정한 시간 가져오기
-        setTime(time);
+        setCurrentTime(currentTime);
     };
 
     const handleTime = () => {
         setIsTime(!isTime);
+    };
+
+    const handleClose = () => {
+        setIsTime(false);
+    };
+
+    const handleSaveTime = () => {
+        // TODO save time api
+        // 성공적으로 저장되면 close
+        setIsTime(false);
+        setPrevTime(currentTime);
     };
 
     return (
@@ -38,11 +50,26 @@ export default function AlarmPage() {
                             className={`flex item-center ${isTime ? 'text-accent' : 'text-gray-900'}`}
                             onClick={handleTime}
                         >
-                            {time}
+                            {prevTime}
                         </div>
                     </div>
                     <div className={`w-full mt-5 ${isTime ? 'block' : 'hidden'}`}>
-                        <ScrollablePicker time={time} setTime={setTime} />
+                        <ScrollablePicker time={currentTime} setTime={setCurrentTime} />
+                    </div>
+                    <div className="h-5"></div>
+                    <div className={`w-full flex gap-2 ${isTime ? 'block' : 'hidden'}`}>
+                        <div
+                            className="flex justify-center items-center w-full h-12 bg-gray-50 text-gray-900 rounded-lg pointer"
+                            onClick={handleClose}
+                        >
+                            취소
+                        </div>
+                        <div
+                            className="flex justify-center items-center w-full h-12 bg-gray-900 text-white rounded-lg pointer"
+                            onClick={handleSaveTime}
+                        >
+                            저장
+                        </div>
                     </div>
                     <div className="h-5"></div>
                     <span className="body2 text-gray-600">‘설정’에서 낫투두클럽 앱의 알람 받기를 켜주세요.</span>
