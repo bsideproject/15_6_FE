1b1b83240b58a0e3d56463e57c1981e59832b4e3
kyu <2066apple@naver.com>
2023-07-23 11:21:22 +0900

    [ docs ] 스토리북 문서 수정

--- src/components/popup/Popup.stories.tsx
+++ src/components/popup/Popup.stories.tsx
@@ -3,6 +3,11 @@ import type { Meta } from '@storybook/react';
 
 import { Popup } from './Popup';
 import { AlertPopup, DeleteTitlePopup } from './PopupGroup';
+import { BottomPopup as BPopup } from './BottomPopup';
+import { Input } from '@/components/common/input/Input';
+import { ReactComponent as Plus } from '@/assets/img/icn_plus.svg';
+import { ReactComponent as Good } from '@/assets/img/icn_thumb_up.svg';
+import { ReactComponent as Bad } from '@/assets/img/icn_thumb_down.svg';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -93,6 +98,101 @@ export const TitlePopup = () => {
                 message={<span>타이틀 팝업 입니다.</span>}
                 title="Title"
             />
+        </>
+    );
+};
+
+export const BottomPopup = () => {
+    const [isSuccess, setIsSuccess] = useState<boolean>(true);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>('');
+
+    const handleOpen = () => {
+        setIsOpen(true);
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    return (
+        <>
+            <button className="w-[100px] h-[30px] rounded-xl bg-primary-light" onClick={handleOpen}>
+                바텀 팝업
+            </button>
+            <BPopup isOpen={isOpen} setIsOpen={setIsOpen}>
+                <div className="w-full h-auto flex justify-end mb-6">
+                    <Plus className="rotate-45" fill="#A2A2A2" />
+                </div>
+                <div className="w-full h-12 rounded-lg flex bg-gray-50 relative">
+                    <div
+                        className={`w-1/2 h-full absolute top-0 rounded-lg transition-all ${
+                            isSuccess ? 'bg-postive left-0' : 'bg-negative left-1/2'
+                        }`}
+                    />
+                    <button
+                        className={`w-full z-10 ${isSuccess ? 'text-gray-900' : 'text-gray-500'}`}
+                        onClick={() => setIsSuccess(true)}
+                    >
+                        성공 기록
+                    </button>
+                    <button
+                        className={`w-full z-10 ${isSuccess ? 'text-gray-500' : 'text-gray-900'}`}
+                        onClick={() => setIsSuccess(false)}
+                    >
+                        실패 기록
+                    </button>
+                </div>
+                <div className="h-4"></div>
+                <Input
+                    type="textarea"
+                    value={inputValue}
+                    setValue={setInputValue}
+                    onChange={handleChange}
+                    placeHolder="코멘트 입력 (최소 4자)"
+                    maxLength={400}
+                    rows={3}
+                    isScroll
+                />
+                <div className="h-10"></div>
+                <button className="w-full h-[48px] bg-gray-900 rounded-lg text-gray-0">완료</button>
+            </BPopup>
+        </>
+    );
+};
+
+export const BottomDetailPopup = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const handleOpen = () => {
+        setIsOpen(true);
+    };
+
+    return (
+        <>
+            <button className="w-[100px] h-[30px] rounded-xl bg-primary-light" onClick={handleOpen}>
+                바텀 디테일 팝업
+            </button>
+            <BPopup isOpen={isOpen} setIsOpen={setIsOpen}>
+                <div className="w-full h-auto flex justify-end mb-6">
+                    <Plus className="rotate-45" fill="#A2A2A2" />
+                </div>
+                <div className="h-7"></div>
+                <div className="w-full flex justify-between">
+                    <div className="flex title2 gap-2">
+                        <Good fill="#73EF5F" />
+                        <span>성공 기록</span>
+                    </div>
+                    <span className="body2 text-gray-500">11:59 pm</span>
+                </div>
+                <div className="h-5"></div>
+                <div className="body1">message</div>
+                <div className="h-10"></div>
+                <div className="flex gap-2">
+                    <button className="w-full h-[48px] title2 bg-gray-50 rounded-lg text-negative">삭제</button>
+                    <button className="w-full h-[48px] title2 bg-gray-50 rounded-lg text-gray-900">수정</button>
+                </div>
+            </BPopup>
         </>
     );
 };
