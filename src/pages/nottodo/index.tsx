a3143554ad4226fa5f637bc13b19dde52c424460
kyu <2066apple@naver.com>
2023-08-06 10:34:46 +0900

    [ fix ] build 에러 수정

--- src/pages/nottodo/index.tsx
+++ src/pages/nottodo/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Card } from '@/components/card/Card';
 import { FloatingButton } from '@/components/buttons/floating/FloatingButton';
